b1b15a9a7ae92e54b149a3474f06b7a220c5cabe
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2024-11-08 11:13:58 +0700

    Remove colorization

--- miden.sh
+++ miden.sh
@@ -5,9 +5,9 @@ PURPLE="\e[38;2;126;90;198m"
 RESET="\e[0m"
 
 # Function to print messages in purple with a newline under them
-print_purple() {
+print() {
     echo ""
-    echo -e "${PURPLE}$1${RESET}"
+    echo "$1"
     echo ""
 }
 
@@ -18,7 +18,7 @@ command_exists() {
 
 # Function to install Rust
 install_rust() {
-    print_purple "Installing Rust..."
+    print "Installing Rust..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     # Source the environment to make Rust available in the current session
     source "$HOME/.cargo/env"
@@ -26,58 +26,65 @@ install_rust() {
 
 # Check if Rust is installed
 if command_exists rustc; then
-    print_purple "✅ Rust is installed."
+    print "✅ Rust is installed."
     # Update Rust to the latest version
-    print_purple "🔄 Updating Rust to the latest version..."
+    print "🔄 Updating Rust to the latest version..."
     rustup update
 else
-    print_purple "❌ Rust is not installed."
+    print "❌ Rust is not installed."
     read -p "$(echo -e "${PURPLE}Do you want to install Rust? [Y/n]${RESET} ")" -n 1 -r
     echo    # Move to a new line
     if [[ $REPLY =~ ^[Yy]$ || -z $REPLY ]]; then
         install_rust
     else
-        print_purple "⚠️ Rust is required to proceed. Exiting."
+        print "⚠️ Rust is required to proceed. Exiting."
         exit 1
     fi
 fi
 
 # Ensure cargo is in PATH
 if ! command_exists cargo; then
-    print_purple "ℹ️ Cargo is not found in PATH. Adding it..."
+    print "ℹ️ Cargo is not found in PATH. Adding it..."
     source "$HOME/.cargo/env"
 fi
 
 # Check if 'miden' directory already exists
 if [ -d "miden" ]; then
-    print_purple "❌ The 'miden' directory already exists. Please remove it."
+    print "❌ The 'miden' directory already exists. Please remove it."
     exit 1
 fi
 
 # Create miden directory and navigate into it
-print_purple "📁 Creating and entering the 'miden' directory..."
+print "📁 Creating and entering the 'miden' directory..."
 mkdir -p miden
 
 cd miden
 
 # Install miden-cli with specified features
-print_purple "⬇️ Installing miden-cli..."
+print "⬇️ Installing miden-cli..."
 cargo install miden-cli --features testing,concurrent
 
 # Ensure miden-cli was installed successfully
 if ! command_exists miden; then
-    print_purple "❌ Failed to install miden-cli. Exiting."
+    print "❌ Failed to install miden-cli. Exiting."
     exit 1
 fi
 
 # Initialize miden with the provided RPC address
-print_purple "🔧 Initializing miden..."
+print "🔧 Initializing miden..."
 miden init --rpc 18.203.155.106
 
 # Create a new mutable wallet
-print_purple "💼 Creating a new mutable wallet..."
+print "💼 Creating a new mutable wallet..."
 miden new-wallet --mutable
 
+print "✅ Setup complete!"
+
+print "🔗 Next steps go to:"
+print "Testnet faucet -> https://testnet.miden.io/"
+print "Documentation -> https://docs.polygon.technology/miden/"
+print "Codebase -> https://github.com/0xPolygonMiden"
+print "Playground -> https://0xpolygonmiden.github.io/examples/"
 
 # Welcome user
 echo -e "${PURPLE} __        __   _                            _          ${RESET}" 
@@ -86,16 +93,8 @@ echo -e "${PURPLE}  \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ \` _ \\ / _ \\ | __/ _ \\
 echo -e "${PURPLE}   \\ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |\\/| || || |) | _| | .\` |   ${RESET}"
 echo -e "${PURPLE}    \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/  |_|  |_|___|___/|___||_|\\_|   ${RESET}"
 
-print_purple "Fasten your seatbelts! We're about to take off into the world of privacy!"
+print "Fasten your seatbelts! We're about to take off into the world of privacy!"
 
 # List accounts
-print_purple "📄 Listing accounts..."
+print "📄 Listing accounts..."
 miden account -l
-
-print_purple "✅ Setup complete!"
-
-print_purple "🔗 Next steps go to:"
-print_purple "Testnet faucet -> https://testnet.miden.io/"
-print_purple "Documentation -> https://docs.polygon.technology/miden/"
-print_purple "Codebase -> https://github.com/0xPolygonMiden"
-print_purple "Playground -> https://0xpolygonmiden.github.io/examples/"
